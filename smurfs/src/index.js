4f88f159aebbf2b72575b7081a63a127d5509a54
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2019-10-11 08:43:38 +0100

    index.js provider, combineReducers, store and applyMiddleware

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import * as reducers from './state/reducers';
 import "./index.css";
 import App from "./components/App";
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+const godReducer = combineReducers({
+    smurfs: reducers.smurfReducer,
+})
+
+const store = createStore(
+    godReducer,
+    applyMiddleware(thunk)
+);
+
+ReactDOM.render(<>
+<Provider store={store}>
+<App />
+</Provider>
+</>, document.getElementById("root"));
